304dee13df326f7fab51aa5b8f71786f06797ece
hernan-gava <de.software1524@gmail.com>
2021-09-24 01:36:07 -0500

    Inicio de Cuentitas-GaVa V1

---
+++ src/main/java/com/cuentitas/gava/mx/CuentitasGaVaApplication.java
@@ -0,0 +1,13 @@
+package com.cuentitas.gava.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentitasGaVaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentitasGaVaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cuentitas/gava/mx/CuentitasGaVaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuentitas.gava.mx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentitasGaVaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
