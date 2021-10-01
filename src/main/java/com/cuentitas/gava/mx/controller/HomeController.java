b2afb4765e1d4d8e6ebea60b0ff978888a93824f
hernan-gava <de.software1524@gmail.com>
2021-10-01 15:06:35 -0500

    Preparando Controller Principal

--- src/main/java/com/cuentitas/gava/mx/controller/HomeController.java
+++ src/main/java/com/cuentitas/gava/mx/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.cuentitas.gava.mx.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 /**
  *
  * @author hernan
