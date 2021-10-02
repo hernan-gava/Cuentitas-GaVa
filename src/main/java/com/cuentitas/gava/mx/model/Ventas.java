8afcd15a7c6e0f192bc5f05efce421b1a18f3141
hernan-gava <de.software1524@gmail.com>
2021-10-01 23:54:28 -0500

    Ventas model

---
+++ src/main/java/com/cuentitas/gava/mx/model/Ventas.java
@@ -0,0 +1,67 @@
+package com.cuentitas.gava.mx.model;
+
+import java.sql.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ventas")
+public class Ventas {
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_ventas")
+    private int idVentas;
+
+    @Column(name = "descripcion_general")
+    private String descripcion;
+
+    @Column(name = "total")
+    private double total;
+
+    @Column(name = "fecha")
+    private Date fecha;
+
+    public Ventas() {
+        Ventas ventas;
+    }
+
+    public int getIdVentas() {
+        return this.idVentas;
+    }
+
+    public void setIdVentas(int idVentas) {
+        this.idVentas = idVentas;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getTotal() {
+        return this.total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Date getFecha() {
+        return this.fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+}
