89ef6681383ceb28837f6597b939e9bde4c0de44
hernan-gava <de.software1524@gmail.com>
2021-10-16 23:18:55 -0500

    Creacion de model pago empleado por mes

---
+++ src/main/java/com/cuentitas/gava/mx/model/PagoMensuales.java
@@ -0,0 +1,55 @@
+package com.cuentitas.gava.mx.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="pago_mensual")
+public class PagoMensuales {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pago")
+    private int idPago;
+
+    @Column(name = "total_pago")
+    private double totalPago;
+
+    @ManyToOne(optional = false)
+	@JoinColumn(name = "id_empleado", nullable = false)
+    private Empleado empleado;
+
+    public PagoMensuales(){}
+    
+    public int getIdPago() {
+        return this.idPago;
+    }
+
+    public void setIdPago(int idPago) {
+        this.idPago = idPago;
+    }
+
+    public double getTotalPago() {
+        return this.totalPago;
+    }
+
+    public void setTotalPago(double totalPago) {
+        this.totalPago = totalPago;
+    }
+
+    public Empleado getEmpleado() {
+        return this.empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+
+}
