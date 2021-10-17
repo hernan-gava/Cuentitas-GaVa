099cb8f258381133b72c3edb046c70a7a5d6091f
hernan-gava <de.software1524@gmail.com>
2021-10-16 23:33:05 -0500

    Creacion clase Dto pago mensual empleado

---
+++ src/main/java/com/cuentitas/gava/mx/model/dto/PagoMensualDto.java
@@ -0,0 +1,22 @@
+package com.cuentitas.gava.mx.model.dto;
+
+import com.cuentitas.gava.mx.model.Empleado;
+
+public class PagoMensualDto {
+     
+    private int idPago;    
+    private String totalPago;
+    private Empleado empleado;
+
+    public int getIdPago() {
+        return this.idPago;
+    }
+
+    public String getTotalPago() {
+        return this.totalPago;
+    }
+
+    public Empleado getEmpleado() {
+        return this.empleado;
+    }
+}
