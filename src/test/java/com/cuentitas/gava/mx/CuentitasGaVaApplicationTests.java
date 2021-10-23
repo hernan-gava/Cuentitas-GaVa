a40bb5c970e1e7eebbd501520e684b0ffa70418f
hernan-gava <de.software1524@gmail.com>
2021-10-23 14:39:02 -0500

    Se crea una prueba de code insertar empleado

--- src/test/java/com/cuentitas/gava/mx/CuentitasGaVaApplicationTests.java
+++ src/test/java/com/cuentitas/gava/mx/CuentitasGaVaApplicationTests.java
@@ -1,13 +1,34 @@
 package com.cuentitas.gava.mx;
 
+import com.cuentitas.gava.mx.exceptions.EmpleadoExceptions;
+import com.cuentitas.gava.mx.model.Empleado;
+import com.cuentitas.gava.mx.service.EmpleadoService;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CuentitasGaVaApplicationTests {
+	
+	@Autowired
+	EmpleadoService empleadoService;		
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test 
+	@DisplayName("Creacion de Empleado")
+	void CrearEmpleado() throws EmpleadoExceptions {		
+		Empleado empleado = new Empleado();
+		
+		empleado.setNombre("Hernan");
+		empleado.setApellidoPaterno("Garcia");
+		empleado.setApellidoMaterno("Valladares");
+		empleado.setDireccion("Sultepec");		
+
+		empleadoService.guardarEmpleado(empleado);
+	}
 }
